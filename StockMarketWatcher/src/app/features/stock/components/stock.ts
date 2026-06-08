f8badfaa72e5f33821863e2b78f1854cd19c9209
Use <jordancw7199@gmail.com>
2026-06-08 17:23:42 -0400

    added ChartJS to project. Added graph to Stock viewing page

--- StockMarketWatcher/src/app/features/stock/components/stock.ts
+++ StockMarketWatcher/src/app/features/stock/components/stock.ts
@@ -1,13 +1,62 @@
- import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild, AfterViewInit } from '@angular/core';
+import { Chart, registerables } from 'chart.js';
 
- @Component({
-    selector: 'app-stock',
-    templateUrl: './stock.html',
-    styleUrl: './stock.css'
-    
+Chart.register(...registerables)
+@Component({
+   selector: 'app-stock',
+   templateUrl: './stock.html',
+   styleUrl: './stock.css',
  })
 
 
- export class Stock {
-
+export class Stock implements AfterViewInit {
+   @ViewChild('myChart', { static: false }) myChart!: ElementRef;
+   public type = 'bar';
+   private chart!: Chart; 
+   private data = {
+                labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+                datasets: [{
+                    label: '# of Votes',
+                    data: [12, 19, 3, 5, 2, 3],
+                    borderWidth: 1
+                }]
+                }
+    private options = {
+        scales: {
+            y: {
+            beginAtZero: true
+            }
+        }
+        }
+   changeChartType(){
+      console.log(this.type);
+      this.chart.destroy();
+    
+      if(this.type == 'bar')
+      {
+        this.chart = new Chart(this.myChart.nativeElement, {
+            type: 'line',
+            data: this.data,
+            options: this.options
+        })
+        this.type = 'line'
+      }
+      else
+      {
+        this.chart = new Chart(this.myChart.nativeElement, {
+            type: 'bar',
+            data: this.data,
+            options: this.options
+        })
+        this.type = 'bar'
+      }
+      
+   }
+   ngAfterViewInit(){
+    this.chart = new Chart(this.myChart.nativeElement, {
+        type: 'bar',
+        data: this.data ,
+        options: this.options
+    });
+   }
  }
